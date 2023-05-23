3856fabfc468e102c0a90be6a4fbc47cb182a4fb
Mark Wanjau <theafrikanson@gmail.com>
2023-05-23 16:24:28 +0300

    add comment on method invocation

--- methods.rb
+++ methods.rb
@@ -18,3 +18,33 @@ def halve(number)
     number / 2 if number.is_a?(Integer)
 end
 
+=begin
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  greet(name)
+end
+
+def add(num1, num2)
+  num1 + num2
+end
+
+def halve(number)
+  number / 2 if number.is_a?(Integer)
+end
+
+# Method invocations
+greet_programmer
+greet("John")
+greet_with_default("Alice")
+puts add(3, 5)
+puts halve(10)
+puts halve(7.5)
+
+=end
