942e34663e6e50fd0a828414a759dd1a394be7c9
Mark Wanjau <theafrikanson@gmail.com>
2023-05-23 15:27:46 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
-# Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+  
+def greet(param)
+    puts "Hello, #{param}!"
+end
+  
+def greet_with_default(param = "programmer")
+    puts "Hello, #{param}!"
+end
+
+def add(a, b)
+    return a + b
+end
+  
+def halve(param)
+    return param / 2 unless param.nil?
+    return nil
+end
